3a3dd0599d8cea06c60a863d37649ae6fd084629
Tim VanFosson <tvanfosson@gmail.com>
2014-03-01 11:36:38 -0600

    Initial check in of code developed for solution

---
+++ Core.Tests/CastAsTypeTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VanFosson.EnumerableExtensions.Core.Tests
+{
+    [TestClass]
+    public class CastAsTypeTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void When_the_source_is_null_an_exception_is_thrown()
+        {
+            IEnumerable<object> source = null;
+
+            source.CastAsType(typeof(string));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void When_the_type_is_null_an_exception_is_thrown()
+        {
+            IEnumerable<object> source = new List<object>();
+
+            source.CastAsType(null);
+        }
+
+        [TestMethod]
+        public void When_the_type_can_be_cast_the_list_is_converted()
+        {
+            IEnumerable<object> source = new List<string> { "test" , "more" };
+
+            var enumerable = source.CastAsType(typeof(string));
+
+            foreach (var obj in enumerable)
+            {
+                Assert.IsInstanceOfType(obj, typeof(string));
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void When_the_type_cannot_be_cast_an_exception_is_thrown_when_enumerated()
+        {
+            IEnumerable<object> source = new List<string> { "test", "more" };
+
+            var enumerable = source.CastAsType(typeof(int));
+
+            foreach (var obj in enumerable)
+            {
+                Assert.Fail("Type {0} could be cast", obj.GetType());
+            }
+        }
+    }
+}
